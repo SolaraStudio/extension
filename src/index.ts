c9b802d05481996dd39a8ff8cdf2f727a321052c
Frostre_ <n9043395@gmail.com>
2026-08-27 07:47:57 +0200

    Create index.ts to export core modules and types

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { ExtensionManager } from './core/ExtensionManager';
+export { ExtensionLoader } from './core/ExtensionLoader';
+export { KotlinBridge } from './bridge/KotlinBridge';
+export * from './types';
